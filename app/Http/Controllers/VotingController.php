f55036bca3b93281102fbf94447c1cdbb0cfd7c9
Subeg7 <subeg7@gmail.com>
2019-01-27 14:44:47 +0545

    voting query parsing complete

--- app/Http/Controllers/VotingController.php
+++ app/Http/Controllers/VotingController.php
@@ -11,24 +11,43 @@ class VotingController extends Controller
     public function ParseVoteQuery(Request $request){
         $query= explode(" ",$request->text);
         $allMainKeys = pull_main_key::pluck('name','id')->all();
-        // var_dump($allMainKeys);
-        // false !== $key = array_search($query[1], $allSubKeys)
+        $mainKey_id;
+        $mainKey;
         if (false !== $mainKey_id = array_search($query[0], $allMainKeys)) {
-            // echo $allMainKeys[$mainKey_id]." has id";
-            $allSubKeys =pull_sub_keys::where('main_key_id',$mainKey_id)->pluck('name','id')->all();
-            // print_r($allSubKeys);
-            if (false !== $subKey_id = array_search($query[1], $allSubKeys)) {
-              // echo"<br>Both subkeys and main key matched";
-              $subKey = pull_sub_keys::find($subKey_id);
-              $subKey->count+=1;
-              $subKey->save();
-              return response([
-                  'success_message' => $subKey->sucess_message,
-                  'count' =>$subKey->count
-              ], 200);
-            }else{
-              echo"this is syntax failure message";
-          }
+          $mainKey = pull_main_key::find($mainKey_id);
+
+            // main key is present but may be disabled by user/reseller
+            if($mainKey->user_enable_status && $mainKey->reseller_enable_status){
+
+                $allSubKeys =pull_sub_keys::where('main_key_id',$mainKey_id)->pluck('name','id')->all();
+                if (false !== $subKey_id = array_search($query[1], $allSubKeys)) {
+                  $subKey = pull_sub_keys::find($subKey_id);
+                  $subKey->count+=1;
+                  $subKey->save();
+                  return response([
+                      'message' => $subKey->sucess_message,
+                      'count' =>$subKey->count
+                  ], 200);
+                }else{
+                  //subkey not found i.e probably syntax error
+                  return response([
+                      'message' =>$mainKey->failure_message,
+                  ], 400);
+
+              }
+
+        }else{
+          //the main key is disabled
+          return response([
+              'message' => $mainKey->disable_message,
+          ], 403);//forbidden
+        }
+        }else{
+          //subkey not found i.e probably syntax error
+          return response([
+              'message' => "Oops! main key not found"
+          ], 400);
+
         }
 
 
